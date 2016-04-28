11063fa30be9899ef044e889e4f2764fb22c8433
Nasser Rafie <contact@geminorum.ir>
2016-04-28 14:16:59 +0430

    :package: 0.1.0 persian-sub-encoding

---
+++ persian-sub-encoding/README.md
@@ -0,0 +1,5 @@
+# [Brothers in Code](https://github.com/brothersincode): [persian-sub-encoding](https://www.npmjs.com/package/persian-sub-encoding)
+
+checks for mis-configured character encoding of the text files
+
+a module from [Persian Sub](https://github.com/brothersincode/persian-sub) package
---
+++ persian-sub-encoding/index.js
@@ -0,0 +1,30 @@
+var isUtf8 = require('is-utf8'); // https://github.com/wayfind/is-utf8
+var iconv = require('iconv-lite'); // https://github.com/ashtuchkin/iconv-lite
+var jschardet = require('jschardet'); // https://github.com/aadsm/jschardet
+
+module.exports = function(contents, options) {
+	options = options || {};
+	
+	if (isUtf8(contents))
+		return contents;
+
+	var encInfo = jschardet.detect(contents);
+	var encFrom = encInfo.encoding;
+
+	switch (encInfo.encoding) {
+		case 'UTF-16LE':
+			encFrom = 'utf16-le';
+			break;
+		default:
+			encFrom = 'win1256';
+	}
+
+	try {
+		var decoded = iconv.decode(contents, encFrom);
+		contents = iconv.encode(decoded, 'utf8');
+	} catch (e) {
+		console.log('Conversion Failed: ' + e);
+	}
+
+	return contents;
+};
